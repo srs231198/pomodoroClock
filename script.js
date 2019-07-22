64de2066d15430bdda1cec26b2de3609297f8b4d
Shreyas Raj Singh <srs231198@gmail.com>
2019-07-23 01:25:54 +0530

    base clock complete
    The basic clock is ready! Now going to add the short and long break sections

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ var resetBtn = document.querySelector("#reset");
 //start button for the timer
 startBtn.addEventListener("click", function() {
     var minuteCounter = 0;
-    var secondCounter = 0;
     var minutes = Number(document.querySelector("#minutes").innerHTML);
     var seconds = Number(document.querySelector("#seconds").innerHTML);
+
+    if(minutes === 25 && seconds === 0){
+        minutes--;
+        document.querySelector("#minutes").innerHTML = minutes.toString();
+        seconds = 59;
+        document.querySelector("#seconds").innerHTML = seconds.toString();
+    }
 
     //countdown minutes
     var minute_time = setInterval(function() {
@@ -24,17 +30,6 @@ startBtn.addEventListener("click", function() {
             document.querySelector("#minutes").innerHTML = minutes.toString();
         }
 
-        //countdown seconds
-        var second_time = setInterval(function(){
-
-            
-
-            secondCounter++
-            if(secondCounter === 59){
-                clearInterval(second_time);
-            }
-        }, 1000);
-
         minuteCounter++;
         if(minuteCounter === 25) {
             clearInterval(minute_time);
@@ -42,9 +37,34 @@ startBtn.addEventListener("click", function() {
 
     }, 60000);
 
-    stopBtn.onclick = function() {clearInterval(minute_time);};
+    //countdown seconds
+    var second_time = setInterval(function(){
+            if(seconds <= 0){
+                seconds = 59;
+                document.querySelector("#seconds").innerHTML = seconds.toString();
+            }
+            else if(seconds <= 10){
+                seconds--;
+                document.querySelector("#seconds").innerHTML = "0" + seconds.toString();  
+            }
+            else{
+                seconds--;
+                document.querySelector("#seconds").innerHTML = seconds.toString();
+            }   
+    }, 1000);
+
+    stopBtn.onclick = function() {
+        clearInterval(minute_time); 
+        clearInterval(second_time);
+    };
+
+    resetBtn.onclick = function() {
+        clearInterval(minute_time); 
+        clearInterval(second_time);
+        document.querySelector("#minutes").innerHTML = "25";
+        document.querySelector("#seconds").innerHTML = "00";
+    }
 });
 
-// stopBtn.addEventListener("click", function() {
-// });
+
 
